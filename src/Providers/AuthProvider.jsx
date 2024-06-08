eee07b2025ca4d164d9a6451044eb6184ceef633
ShariarArefinOfficial <shariararefin0@gmail.com>
2024-06-09 04:34:35 +0600

    firebase auth provider 50% done

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -1,10 +1,56 @@
 //import React from 'react';
+import { getAuth } from "firebase/auth";
+import app from "../firebase/firebase.config";
+import { createContext, useState } from "react";
+import { GoogleAuthProvider, createUserWithEmailAndPassword,  onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth"
 
-const AuthProvider = () => {
+export const AuthContext = createContext(null);
+const auth = getAuth(app);
+
+const AuthProvider = ({ children }) => {
+    const googleProvider = new GoogleAuthProvider();
+    const [user,setUser]=useState(null)
+    const [loading,setLoading]=useState(false)
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    const signIn = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    const googleSignIn = () => {
+        setLoading(true);
+        return signInWithPopup(auth, googleProvider);
+    }
+
+    const logOut = () => {
+        setLoading(true);
+        return signOut(auth);
+    }
+
+    const updateUserProfile = (name, photo) => {
+        return updateProfile(auth.currentUser, {
+            displayName: name, photoURL: photo
+        });
+    }
+
+
+    const authInfo = {
+        user,
+        loading,
+        createUser,
+        signIn,
+        logOut,
+        updateUserProfile,
+        googleSignIn
+    }
     return (
-        <div>
-            
-        </div>
+        <AuthContext.Provider value={authInfo}>
+        {children}
+       </AuthContext.Provider>
     );
 };
 
